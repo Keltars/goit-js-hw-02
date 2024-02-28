4bcbee32b7e588c274b9ec1b3c29307caa4b2f45
Keltars <keltars2000@gmail.com>
2024-02-28 08:49:50 +0200

    js-hw-02-commit-01

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,12 @@
-function getElementWidth(content, padding, border) { 
-const numericalContent = Number.parseFloat(content);
-const numericalPadding = Number.parseFloat(padding);
-const numericalBorder = Number.parseFloat(border);
-    return numericalContent + numericalPadding * 2 + numericalBorder * 2;
+function checkForSpam(message) {
+  if (message.toLowerCase().includes('spam')) {
+    return true;
+  } else if (message.toLowerCase().includes('sale')) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam('Trust me, this is not a spam message'));
+console.log(checkForSpam('Latest technology news'));
